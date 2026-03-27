f997d4ecc0da5a044468351dc7c06b23b440b8c6
AmstaLabestar <hamzabikienga07@gmail.com>
2026-03-27 14:38:33 +0000

    feat(offers): add merchant offer creation and listing flow

--- src/modules/offer/offer.routes.js
+++ src/modules/offer/offer.routes.js
@@ -1,10 +1,46 @@
 const express = require('express');
 
+const { authMiddleware, requireRole } = require('../../middlewares/auth.middleware');
 const { asyncHandler } = require('../../utils/async-handler');
+const { validate } = require('../../utils/validators');
 const offerController = require('./offer.controller');
+const { buildOfferContainer } = require('./offer.container');
+const { createOfferSchema, updateOfferStatusSchema } = require('./offer.validation');
 
 const router = express.Router();
 
-router.get('/', asyncHandler(offerController.listOffers));
+router.use((req, _res, next) => {
+  req.container = buildOfferContainer();
+  next();
+});
+
+router.get('/', authMiddleware, asyncHandler(async (req, res) => offerController.listOffers(req, res)));
+
+router.get(
+  '/mine',
+  authMiddleware,
+  requireRole('MERCHANT', 'ADMIN'),
+  asyncHandler(async (req, res) => offerController.listMyOffers(req, res)),
+);
+
+router.post(
+  '/',
+  authMiddleware,
+  requireRole('MERCHANT', 'ADMIN'),
+  asyncHandler(async (req, res) => {
+    req.body = validate(createOfferSchema, req.body);
+    return offerController.createOffer(req, res);
+  }),
+);
+
+router.patch(
+  '/:offerId/status',
+  authMiddleware,
+  requireRole('MERCHANT', 'ADMIN'),
+  asyncHandler(async (req, res) => {
+    req.body = validate(updateOfferStatusSchema, req.body);
+    return offerController.updateOfferStatus(req, res);
+  }),
+);
 
 module.exports = router;
