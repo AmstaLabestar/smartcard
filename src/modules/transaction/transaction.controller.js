bde6bd07744203ee6f776b6d1a2a345165ef50d6
AmstaLabestar <hamzabikienga07@gmail.com>
2026-03-27 15:06:41 +0000

    feat(transactions): add scan and discount transaction flow

--- src/modules/transaction/transaction.controller.js
+++ src/modules/transaction/transaction.controller.js
@@ -1,9 +1,43 @@
-async function listTransactions(_req, res) {
-  res.status(200).json({
-    success: true,
-    message: 'Transactions endpoint ready',
-    data: [],
+const { createSuccessResponse } = require('../../utils/api-response');
+
+async function scanTransaction(req, res) {
+  const transaction = await req.container.transactionService.scanAndCreateTransaction({
+    requester: req.user,
+    payload: req.body,
   });
+
+  res.status(201).json(
+    createSuccessResponse({
+      message: 'Transaction created successfully',
+      data: transaction,
+    }),
+  );
 }
 
-module.exports = { listTransactions };
+async function listMyTransactions(req, res) {
+  const transactions = await req.container.transactionService.listMyTransactions(req.user.sub);
+
+  res.status(200).json(
+    createSuccessResponse({
+      message: 'User transactions fetched successfully',
+      data: transactions,
+    }),
+  );
+}
+
+async function listMerchantTransactions(req, res) {
+  const transactions = await req.container.transactionService.listMerchantTransactions(req.user);
+
+  res.status(200).json(
+    createSuccessResponse({
+      message: 'Merchant transactions fetched successfully',
+      data: transactions,
+    }),
+  );
+}
+
+module.exports = {
+  scanTransaction,
+  listMyTransactions,
+  listMerchantTransactions,
+};
--- src/modules/transaction/transaction.routes.js
+++ src/modules/transaction/transaction.routes.js
@@ -1,10 +1,36 @@
 const express = require('express');
 
+const { authMiddleware, requireRole } = require('../../middlewares/auth.middleware');
 const { asyncHandler } = require('../../utils/async-handler');
+const { validate } = require('../../utils/validators');
 const transactionController = require('./transaction.controller');
+const { buildTransactionContainer } = require('./transaction.container');
+const { scanTransactionSchema } = require('./transaction.validation');
 
 const router = express.Router();
 
-router.get('/', asyncHandler(transactionController.listTransactions));
+router.use(authMiddleware);
+
+router.use((req, _res, next) => {
+  req.container = buildTransactionContainer();
+  next();
+});
+
+router.post(
+  '/scan',
+  requireRole('MERCHANT', 'ADMIN'),
+  asyncHandler(async (req, res) => {
+    req.body = validate(scanTransactionSchema, req.body);
+    return transactionController.scanTransaction(req, res);
+  }),
+);
+
+router.get('/mine', asyncHandler(async (req, res) => transactionController.listMyTransactions(req, res)));
+
+router.get(
+  '/merchant',
+  requireRole('MERCHANT', 'ADMIN'),
+  asyncHandler(async (req, res) => transactionController.listMerchantTransactions(req, res)),
+);
 
 module.exports = router;
